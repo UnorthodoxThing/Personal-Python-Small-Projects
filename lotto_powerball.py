56d57691bd997092f183cb4c8960481762bd1540
UnorthodoxThing <UnorthodoxThing@Outlook.com>
2017-05-10 12:22:50 +1000

    Create lotto_powerball.py

---
+++ lotto_powerball.py
@@ -0,0 +1,11 @@
+# Personal-Python-Small-Projects
+'''See what I've made with what I learn in python everyday. :) Nothing extraordinary but progress and understand.'''
+
+Picked_up = []
+
+import random
+
+while len(Picked_up) < 6:
+    Picked_up.append(random.randint(0,49))
+
+print Picked_up
